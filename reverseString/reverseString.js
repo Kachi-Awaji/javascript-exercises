a7c75e121dd985b96706123d795af2b16f5c84b2
Kachi Awaji <kachbytes@gmail.com>
2019-05-29 07:31:09 +0100

    Reverse String

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strArr =[], 
+        revArr = [];
+        l = str.length;
 
+    for (let i = 0; i<l; i++){
+        strArr.push(str[i]);
+    }
+
+
+    for ( i =0; i<l; i++){
+        revArr.push(strArr.pop());
+    }
+
+    return revArr.join('');
 }
 
+reverseString("Kachi Awaji");
 module.exports = reverseString
