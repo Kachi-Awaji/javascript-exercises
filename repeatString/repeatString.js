6f9b016a82f68d34b328f3bfd4064c2146499d53
Kachi Awaji <kachbytes@gmail.com>
2019-05-29 07:05:21 +0100

    Completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(s, r) {
+    let output='';   
+    if (r<0){
+        return 'ERROR';
+    }
+    for (let repeat = r; repeat >0; repeat--){
+        output+=s;
+    }
+    return output;
 }
 
 module.exports = repeatString
