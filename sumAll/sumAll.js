03f8725cc17ec4ef8eace9c53486c8b8c0f413c8
Kachi Awaji <kachbytes@gmail.com>
2019-05-30 07:03:04 +0100

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    let sum=0;
+    console.log(typeof(a));
+    console.log(typeof(b));
 
+    if ((a<0)||(b<0)){
+        return 'ERROR';
+    }
+
+    if (typeof(a)!= "number" || typeof(b)!="number"){
+        return 'ERROR';
+    }
+
+    if (a<b){
+        for (let index = a; index <=b; index++){
+            sum += index;
+        }
+    }
+    else{
+        for (let index = b; index <=a; index++){
+            sum += index;
+        }
+    }
+    return sum;
 }
 
+
 module.exports = sumAll
+
